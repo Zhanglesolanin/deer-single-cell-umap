80681bf3934035334ed2359076bb7e4234c2f307
zhanglesolanin <zhangle2@genomics.cn>
2026-05-20 14:05:39 +0800

    Fix itemsizing type error with 'as const'

--- src/components/UMAPView.tsx
+++ src/components/UMAPView.tsx
@@ -227,7 +227,7 @@ const LARGE = {
   autosize: true, margin: { l: 30, r: 30, t: 30, b: 60 },
   paper_bgcolor: "#0d1117", plot_bgcolor: "#161b22",
   font: { color: "#8b949e", size: 11 },
-  legend: { orientation: "h", y: -0.12, x: 0.5, xanchor: "center", font: { size: 11 }, itemsizing: "constant", itemwidth: 20 },
+  legend: { orientation: "h", y: -0.12, x: 0.5, xanchor: "center", font: { size: 11 }, itemsizing: "constant" as const, itemwidth: 20 },
   dragmode: "pan" as const, hovermode: "closest" as const,
 };
 
